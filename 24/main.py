e60b625913cb56a3e50754d29be7f9da5f2eb6b2
Ananya Ayachit <ananya.ayachit@atco.com>
2023-04-12 12:58:11 -0600

    Day 24 Pt 2

--- 24/main.py
+++ 24/main.py
@@ -75,10 +75,149 @@ def part1(lines):
         # for line in eris:
         #     print("".join(line))
         
+def outerCount(generation2D):
+    top_count = 0
+    bottom_count = 0
+    right_count = 0
+    left_count = 0
+    for i in range(5):
+        if generation2D [0][i] == '#':
+            top_count += 1
+        if generation2D [-1][i] == '#':
+            bottom_count += 1
+        if generation2D [i][0] == '#':
+            left_count += 1
+        if generation2D [i][-1] == '#':
+            right_count += 1
+    return [top_count,bottom_count,left_count,right_count]
+
+def innerCount(generation2D):
+    top_count = 0
+    bottom_count = 0
+    right_count = 0
+    left_count = 0
+    
+    if generation2D [1][2] == '#':
+        top_count += 1
+    if generation2D [3][2] == '#':
+        bottom_count += 1
+    if generation2D [2][1] == '#':
+        left_count += 1
+    if generation2D [2][3] == '#':
+        right_count += 1
+    return [top_count,bottom_count,left_count,right_count]
+
+def nextGeneration3D(generation3D):
+    next = []
+    next.append([[".",".",".",".","."],[".",".",".",".","."],[".",".",".",".","."],[".",".",".",".","."],[".",".",".",".","."]])
+    top_count,bottom_count,left_count,right_count = outerCount(generation3D[0])
+    if top_count == 1 or top_count == 2:
+        next[0][1][2] = '#'
+    if bottom_count == 1 or bottom_count == 2:
+        next[0][3][2] = '#'
+    if left_count == 1 or left_count == 2:
+        next[0][2][1] = '#'
+    if right_count == 1 or right_count == 2:
+        next[0][2][3] = '#'
+    for z in range(len(generation3D)):
+        newPlane = []
+        if z > 0:
+            top_count,bottom_count,left_count,right_count = innerCount(generation3D[z-1])
+        else:
+            top_count = 0
+            bottom_count = 0
+            left_count = 0
+            right_count = 0
+        if z < len(generation3D)-1:
+            innertop_count,innerbottom_count,innerleft_count,innerright_count = outerCount(generation3D[z+1])
+        else:
+            innertop_count = 0
+            innerbottom_count = 0
+            innerleft_count = 0
+            innerright_count = 0
+        for y in range(len(generation3D[z])):
+            newLine = []
+            for x in range(len(generation3D[z][0])):
+                neighbours = 0
+                if y > 0 and generation3D[z][y-1][x] == "#":
+                    neighbours += 1
+                if y == 0:
+                    neighbours += top_count
+                if y < len(generation3D[z]) - 1 and generation3D[z][y+1][x] == "#":
+                    neighbours += 1
+                if y == 4: 
+                    neighbours += bottom_count
+                if x > 0 and generation3D[z][y][x-1] == "#":
+                    neighbours += 1
+                if x == 0:
+                    neighbours += left_count
+                if x < len(generation3D[z][0]) - 1 and generation3D[z][y][x+1] == "#":
+                    neighbours += 1
+                if x == 4:
+                    neighbours += right_count
+                if y == 1 and x == 2:
+                    neighbours += innertop_count
+                if y == 3 and x == 2:
+                    neighbours += innerbottom_count
+                if y == 2 and x == 1:
+                    neighbours += innerleft_count
+                if y == 2 and x == 3:
+                    neighbours += innerright_count
+                if y == 2 and x == 2:
+                    neighbours = 10
+
+                if generation3D[z][y][x] == "#":
+                    if neighbours == 1:
+                        newLine.append("#")
+                    else:
+                        newLine.append(".")
+                else:
+                    if neighbours == 1 or neighbours == 2:
+                        newLine.append("#")
+                    else:
+                        newLine.append(".")
+            newPlane.append(newLine)
+        next.append(newPlane)
+    next.append([[".",".",".",".","."],[".",".",".",".","."],[".",".",".",".","."],[".",".",".",".","."],[".",".",".",".","."]])
+    innertop_count,innerbottom_count,innerleft_count,innerright_count = innerCount(generation3D[-1])
+    if innertop_count == 1:
+        for i in range(5):
+            next[-1][0][i] = "#"
+    if innerbottom_count == 1:
+        for i in range(5):
+            next[-1][-1][i] = "#"
+    if innerleft_count == 1:
+        for i in range(5):
+            next[-1][i][0] = "#"
+    if innerright_count == 1:
+        for i in range(5):
+            next[-1][i][-1] = "#"
+
+    return next
+
 
 def part2(lines):
     # Code the solution to part 2 here, returning the answer as a string
-    pass
+    
+    eris = [[]]
+    for line in lines:
+        newLine = []
+        for spot in line:
+            newLine.append(spot)
+        eris[0].append(newLine)
+    for i in range(200):
+        eris = nextGeneration3D(eris)
+    print(eris)
+    count = 0
+    for z in range(len(eris)):
+        for y in range(5):
+            for x in range(5):
+                if eris[z][y][x] == "#":
+                    count += 1
+    print(count)
+
+    
+   
 
 def main ():
     # Opens a dialog to select the input file
